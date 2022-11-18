4d9907ab204fb925eab3ebc440e536ce2f81dfc9
Mikhail <kapellan422@gmail.com>
2022-11-18 14:46:37 +0600

    get elements like variables

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+let btnStartCalc = document.querySelector('#start'),
+
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+
+    expensesItems = document.querySelectorAll('.expenses-item'),
+
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+
+    optionalExpensesItems = document.querySelectorAll('.optionalexpenses-item'),
+
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day =  document.querySelector('.day-value');
